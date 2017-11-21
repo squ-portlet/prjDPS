ba0d8e1618caed9b1ee3812bcf314a24c622a171
Bhabesh <bhabesh@squ.edu.om>
2017-11-21 16:55:09 +0400

    cosmetic changes in comment area of the SEQUENCE

--- doc/db/SEQUENCE/DPS_SQ.sql
+++ doc/db/SEQUENCE/DPS_SQ.sql
@@ -1,5 +1,10 @@
 --------------------------------------------------------
---  DDL for Sequence DPS_SQ
+--  Sequence Name : DPS_SQ
+  
+--  Summary : Sequence  for general DPS related works 
+
+--  Author : Bhabesh
+--  Create Date : 21-November-2017
 --------------------------------------------------------
 
    CREATE SEQUENCE  "DPSPROJECT"."DPS_SQ"  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
