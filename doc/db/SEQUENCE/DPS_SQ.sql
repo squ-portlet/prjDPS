504f31b4b334c8b11238f8c21d9bdad2fbefb8a1
Bhabesh <bhabesh@squ.edu.om>
2017-11-21 13:15:40 +0400

    Sequence created for general purpose

---
+++ doc/db/SEQUENCE/DPS_SQ.sql
@@ -0,0 +1,5 @@
+--------------------------------------------------------
+--  DDL for Sequence DPS_SQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "DPSPROJECT"."DPS_SQ"  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
