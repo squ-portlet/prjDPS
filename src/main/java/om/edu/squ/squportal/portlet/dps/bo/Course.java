c2db3ed8868bda7c01b71d042bbde848263e16ef
Bhabesh <bhabesh@squ.edu.om>
2018-01-13 02:52:15 +0400

    conflict correction in Course.java

--- src/main/java/om/edu/squ/squportal/portlet/dps/bo/Course.java
+++ src/main/java/om/edu/squ/squportal/portlet/dps/bo/Course.java
@@ -246,8 +246,9 @@ public class Course
 	public void setSemester(int semester)
 	{
 		this.semester = semester;
-
-	 * Getter Method	: getGradeValue
+	}
+	
+	/** Getter Method	: getGradeValue
 	 * @return the gradeValue
 	 * 
 	 * Date				: Dec 25, 2017
@@ -278,7 +279,7 @@ public class Course
 				+ ", sectCode=" + this.sectCode + ", sectionNo="
 				+ this.sectionNo + ", yearSemester=" + this.yearSemester
 				+ ", credits=" + this.credits + ", courseYear="
-				+ this.courseYear + ", semester=" + this.semester + "]";
+				+ this.courseYear + ", semester=" + this.semester + "]"
 				+ ", credits=" + this.credits + ", gradeValue="
 				+ this.gradeValue + "]";
 	}
