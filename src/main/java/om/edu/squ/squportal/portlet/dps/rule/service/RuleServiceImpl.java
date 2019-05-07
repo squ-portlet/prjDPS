43982524af9b491f702fde26eeb06a1306c85e8e
Bhabesh <bhabesh@squ.edu.om>
2019-05-07 08:31:41 +0400

    Modification to on total semester count including summer

--- src/main/java/om/edu/squ/squportal/portlet/dps/rule/service/RuleServiceImpl.java
+++ src/main/java/om/edu/squ/squportal/portlet/dps/rule/service/RuleServiceImpl.java
@@ -93,6 +93,11 @@ public class RuleServiceImpl implements Rule
 
 		int s =0;
 		
+		/*
+		 * x = year
+		 * y = semester number within the year (e.g. : 2 / 3 / 4)
+		 * s = semester count including summer
+		 * */
 		for(int x=fromStartYear; x<currYear; x++)
 		{
 			int p = 0;
@@ -110,13 +115,12 @@ public class RuleServiceImpl implements Rule
 			{
 
 				s++;
-				if(s==10) break;
+				if(s>10) break;
 				
 				if(y!=3)
 				{
 					countSem++;
 				}
-				
 			}
 			
 		}
